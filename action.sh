f5c3ef2908e7f127bff74112f862d12a660769c0
Avish Kumar <85635883+aviskumar@users.noreply.github.com>
2021-08-01 20:21:27 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -2,4 +2,4 @@ branch=dev
 git clone -b $branch https://github.com/aviskumar/tgvc-userbot /root/callsmusic
 cd /root/callsmusic
 docker build -t tgcalls .
-docker run -it --rm --env-file ./envfile --name tgvc-userbot tgcalls
+docker run -it --rm --name tgvc-userbot tgcalls
